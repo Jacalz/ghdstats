a1a59e0de6d0172e2efff6e2a167e8731d1b0b74
Jacob Alzén <jacob.alzen@gmail.com>
2021-12-23 17:37:41 +0100

    Initial kind of working concept
    The printing is currently very racy but otherwise working.
    Need to see about hooking into the GitHub Api better to avoid rate limiting.

--- main.go
+++ main.go
@@ -5,16 +5,25 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"sync"
+	"time"
 )
-
-const apiEndpoint = "https://api.github.com/users/"
 
 type repository struct {
 	Name string `json:"full_name"`
 }
 
+type statistics struct {
+	Assets []struct {
+		Name          string    `json:"name"`
+		DownloadCount uint      `json:"download_count"`
+		Date          time.Time `json:"updated_at"`
+	} `json:"assets"`
+}
+
 func fetchRepositories(user string) []repository {
-	resp, err := http.Get(apiEndpoint + user + "/repos")
+	const usersApiEndpoint = "https://api.github.com/users/"
+	resp, err := http.Get(usersApiEndpoint + user + "/repos")
 	if err != nil {
 		panic(err)
 	}
@@ -30,6 +39,51 @@ func fetchRepositories(user string) []repository {
 	return repos
 }
 
+func fetchStatistics(repos []repository) {
+	wg := sync.WaitGroup{}
+	wg.Add(len(repos))
+
+	const reposApiEndpoint = "https://api.github.com/repos/"
+	for _, repo := range repos {
+		go func(repourl, reponame string) {
+			defer wg.Done()
+
+			fmt.Println(reponame, repourl)
+
+			resp, err := http.Get(repourl)
+			if err != nil {
+				panic(err)
+			}
+
+			defer resp.Body.Close()
+			stats := []statistics{}
+			totalDownloads := uint(0)
+
+			err = json.NewDecoder(resp.Body).Decode(&stats)
+			if err != nil {
+				panic(err)
+			}
+
+			for _, stat := range stats {
+				if stat.Assets == nil || len(stat.Assets) == 0 {
+					continue
+				}
+
+				for _, asset := range stat.Assets {
+					totalDownloads += asset.DownloadCount
+					fmt.Printf("Repo: %s\tAsset: %s\tCount: %d\tDate: %s\n", reponame, asset.Name, asset.DownloadCount, asset.Date.Format(time.RFC850))
+				}
+
+				fmt.Println()
+			}
+
+			fmt.Printf("Total downloads for %s: %d\n\n", reponame, totalDownloads)
+		}(reposApiEndpoint+repo.Name+"/releases", repo.Name)
+	}
+
+	wg.Wait()
+}
+
 func main() {
 	args := os.Args[1:]
 	if len(args) == 0 || len(args) > 2 {
@@ -40,10 +94,10 @@ func main() {
 	repos := []repository{}
 
 	if len(args) == 2 {
-		repos = append(repos, repository{apiEndpoint + args[0] + "/" + args[1]})
+		repos = append(repos, repository{args[0] + "/" + args[1]})
 	} else {
 		repos = append(repos, fetchRepositories(args[0])...)
 	}
 
-	fmt.Println(repos)
+	fetchStatistics(repos)
 }
