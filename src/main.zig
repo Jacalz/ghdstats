c2ada874a3de2bf2d6f2931a0d8f44813ceee8e0
Jacob <jacalz@tutanota.com>
2024-03-05 17:04:27 +0100

    Use std.os.argv directly

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,13 @@
 const std = @import("std");
 
 pub fn main() !void {
-    var args = std.process.args();
+    const args = std.os.argv;
+    if (args.len == 0 or args.len > 2) {
+        std.debug.print("Usage: gcdstats [user] [repository, optional]\n");
+        return;
+    }
 
-    while (args.next()) |arg| {
+    while (std.os.argv) |arg| {
         std.debug.print("{s}\n", .{arg});
     }
 }
