34487ed98b2347ab93ca347727d1c652abe21591
Jacalz <jacalz@tutanota.com>
2025-10-08 14:05:50 +0200

    Use clap for parsing arguments

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,26 @@
 mod api;
 use anyhow::Result;
-use std::env;
-use std::process;
+use clap::{Parser, command};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+/// A CLI tool to fetch download statistics for GitHub repositories.
+struct Args {
+    /// The GitHub username to fetch download statistics for.
+    user: String,
+
+    /// Optionally, the specific repository to fetch download statistics for.
+    repo: Option<String>,
+}
 
 fn main() -> Result<()> {
-    let args: Vec<String> = env::args().collect();
-    match args.len() {
-        0 | 1 | 4.. => {
-            eprintln!("Usage: gcdstats [user] [repository, optional]");
-            process::exit(1);
-        }
-        3 => api::Client::print_downloads_for_repo(&format!("{}/{}", &args[1], &args[2])),
-        2 => {
-            let parts: Vec<&str> = args[1].split('/').collect();
-            match parts.len() {
-                1 => api::Client::new()
-                    .lookup_repos(parts[0])?
-                    .print_all_downloads(),
-                2 => api::Client::print_downloads_for_repo(&args[1]),
-                _ => {
-                    eprintln!("Invalid input format");
-                    process::exit(1);
-                }
-            }
-        }
+    let args = Args::parse();
+
+    if let Some(repo) = args.repo {
+        return api::Client::print_downloads_for_repo(&format!("{}/{}", &args.user, &repo));
     }
+
+    return api::Client::new()
+        .lookup_repos(&args.user)?
+        .print_all_downloads();
 }
